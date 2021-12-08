3e248342fd9cc808eaf00801e1843668fee08e5f
“diskup” <taigaa.1998@gmail.com>
2021-12-08 08:06:31 +0000

    deviseインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, skip:[:regstrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users, skip:[:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
 end
