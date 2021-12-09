db219353bb6bbc554b92778c6ed80c2a81df4a77
“diskup” <taigaa.1998@gmail.com>
2021-12-09 15:27:27 +0000

    routing設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,44 @@
 Rails.application.routes.draw do
   devise_for :admin, skip:[:regstrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions'
   }
+  namespace :admin do
+    root to: 'homes#top'
+    get 'search' => 'searches#search', as: 'search'
+    resources :shops,  only:[:index, :show, :edit, :update]
+    resources :fix_requests, only:[:index, :show, :edit, :update]
+  end
+
+  root to: 'user/homes#top'
   devise_for :users, skip:[:passwords], controllers: {
-    registrations: "user/registrations",
-    sessions: "user/sessions"
+    registrations: 'user/registrations',
+    sessions: 'user/sessions'
   }
+  scope module: :user do
+    get 'search' => 'searches#search', as: 'search'
+    resources :shops, except: [:destroy] do
+      get 'confirm' => 'shops#confirm'
+      resources :images, only: [:new, :create, :destroy] do
+        post 'image_favorites' => 'image_favorites#create', as: 'image_favorites'
+        delete 'image_favorites' => 'image_favorites#destroy', as: 'image_favorite'
+      end
+      post 'shop_favorites' => 'shop_favorites#create', as: 'shop_favorites'
+      delete 'shop_favorites' => 'shop_favorites#destroy', as: 'shop_favorite'
+      resources :reviews, only: [:new, :create, :destroy]
+      resources :fix_requests, only: [:new, :create]
+    end
+    resources :timeline, except: [:edit, :update] do
+      post 'timeline_favorites' => 'timeline_favorites#create', as: 'timeline_favorites'
+      delete 'timeline_favorites' => 'timeline_favorites#destroy', as: 'timeline_favorite'
+
+    end
+    resources :schedules, only: [:index, :create, :update, :destroy]
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'users#followings'
+      get 'followeds' => 'users#followeds'
+      get 'unsubscribe' => 'users#unsubscribe'
+      get 'withdraw' => 'users#/withdraw'
+    end
+  end
 end
--- db/migrate/20211208082657_create_fix_requests.rb
+++ db/migrate/20211208082657_create_fix_requests.rb
@@ -5,7 +5,7 @@ class CreateFixRequests < ActiveRecord::Migration[5.2]
       t.integer :shop_id, null: false
       t.integer :fix_status, null: false, default: 0
       t.text :user_message, null: false
-      t.text :admin_mssage, null: 
+      t.text :admin_mssage, null: false
       t.timestamps
     end
   end
