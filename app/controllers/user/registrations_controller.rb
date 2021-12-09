1b73e8b51fc3215828ca95e1e72c251860ee317b
“diskup” <taigaa.1998@gmail.com>
2021-12-09 16:08:17 +0000

    devise設定

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -41,9 +41,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  
+
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :current_prefectures])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -52,9 +52,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
